12736a802355f1bfc64d2634911ef551da25d4e7
Bartek <bartex600700@gmail.com>
2023-06-27 22:49:54 +0200

    1.0

---
+++ main.py
@@ -0,0 +1,28 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("Polish Voivodeships Game")
+image = "Voivodeships_of_Poland.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+pointer = turtle.Turtle()
+pointer.hideturtle()
+pointer.penup()
+pointer.speed("fastest")
+
+data = pandas.read_csv("voivodeships.csv")
+
+i = 1
+while i < 16:
+    answer_voivodeship = screen.textinput(title=f"Guess the voivodeship {i}/16",
+                                          prompt="What's another voivodeship name?")
+    voiv = data[data.voivodeship.str.lower() == answer_voivodeship.lower()]
+    voivodeship_name = voiv.voivodeship.to_string(index=False)
+    x, y = float(voiv.x.to_string(index=False)), float(voiv.y.to_string(index=False))
+    pointer.setposition(x, y)
+    pointer.write(voivodeship_name, font=("Arial", 10, "bold"))
+    i += 1
+
+screen.mainloop()
