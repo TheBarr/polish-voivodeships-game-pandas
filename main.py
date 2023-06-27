5b1cecdad871b6d08bd3701540edf76748a885f2
Bartek <bartex600700@gmail.com>
2023-06-27 23:47:56 +0200

    1.0

--- main.py
+++ main.py
@@ -7,24 +7,28 @@ image = "Voivodeships_of_Poland.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-pointer = turtle.Turtle()
-pointer.hideturtle()
-pointer.penup()
-pointer.speed("fastest")
+
 
 data = pandas.read_csv("voivodeships.csv")
+all_voivodeships = data.voivodeship.to_list()
+guessed_voivodeships = []
 
-for i in range(16):
-    answer_voivodeship = screen.textinput(title=f"Guess the voivodeship {i}/16",
+while len(guessed_voivodeships) < 16:
+    answer_voivodeship = screen.textinput(title=f"Guess the voivodeship {len(guessed_voivodeships)}/16",
                                           prompt="What's another voivodeship name?")
-    if answer_voivodeship in data.voivodeship.to_string() and answer_voivodeship != "":
+    if answer_voivodeship in all_voivodeships:
+        guessed_voivodeships.append(answer_voivodeship)
         voiv = data[data.voivodeship.str.lower() == answer_voivodeship.lower()]
-        voivodeship_name = voiv.voivodeship.to_string(index=False)
-        x, y = float(voiv.x.to_string(index=False)), float(voiv.y.to_string(index=False))
-        pointer.setposition(x, y)
+        voivodeship_name = voiv.voivodeship.item()
+
+        pointer = turtle.Turtle()
+        pointer.hideturtle()
+        pointer.penup()
+        pointer.speed("fastest")
+        pointer.setposition(voiv.x.iloc[0], voiv.y.iloc[0])
         pointer.write(voivodeship_name, font=("Arial", 10, "bold"))
-        i += 1
+
     else:
         continue
 
-screen.mainloop()
+screen.exitonclick()
