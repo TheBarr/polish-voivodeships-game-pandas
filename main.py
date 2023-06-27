fa681f604d262aeefb1fc40e4f9b0b1bd8fef07d
Bartek <bartex600700@gmail.com>
2023-06-28 00:01:12 +0200

    1.0

--- main.py
+++ main.py
@@ -7,18 +7,16 @@ image = "Voivodeships_of_Poland.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-
-
 data = pandas.read_csv("voivodeships.csv")
 all_voivodeships = data.voivodeship.to_list()
 guessed_voivodeships = []
 
-while len(guessed_voivodeships) < 16:
+while len(guessed_voivodeships) < 17:
     answer_voivodeship = screen.textinput(title=f"Guess the voivodeship {len(guessed_voivodeships)}/16",
-                                          prompt="What's another voivodeship name?")
+                                          prompt="What's another voivodeship name?").title()
     if answer_voivodeship in all_voivodeships:
         guessed_voivodeships.append(answer_voivodeship)
-        voiv = data[data.voivodeship.str.lower() == answer_voivodeship.lower()]
+        voiv = data[data.voivodeship == answer_voivodeship]
         voivodeship_name = voiv.voivodeship.item()
 
         pointer = turtle.Turtle()
@@ -27,8 +25,5 @@ while len(guessed_voivodeships) < 16:
         pointer.speed("fastest")
         pointer.setposition(voiv.x.iloc[0], voiv.y.iloc[0])
         pointer.write(voivodeship_name, font=("Arial", 10, "bold"))
-
-    else:
-        continue
 
 screen.exitonclick()
