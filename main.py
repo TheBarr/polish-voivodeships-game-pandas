e7ec030a820a7c0de0f27925199310bbf406d5c3
Bartek <bartex600700@gmail.com>
2023-06-27 23:07:25 +0200

    1.0

--- main.py
+++ main.py
@@ -14,15 +14,17 @@ pointer.speed("fastest")
 
 data = pandas.read_csv("voivodeships.csv")
 
-i = 1
-while i < 16:
+for i in range(16):
     answer_voivodeship = screen.textinput(title=f"Guess the voivodeship {i}/16",
                                           prompt="What's another voivodeship name?")
-    voiv = data[data.voivodeship.str.lower() == answer_voivodeship.lower()]
-    voivodeship_name = voiv.voivodeship.to_string(index=False)
-    x, y = float(voiv.x.to_string(index=False)), float(voiv.y.to_string(index=False))
-    pointer.setposition(x, y)
-    pointer.write(voivodeship_name, font=("Arial", 10, "bold"))
-    i += 1
+    if answer_voivodeship in data.voivodeship.to_string() and answer_voivodeship != "":
+        voiv = data[data.voivodeship.str.lower() == answer_voivodeship.lower()]
+        voivodeship_name = voiv.voivodeship.to_string(index=False)
+        x, y = float(voiv.x.to_string(index=False)), float(voiv.y.to_string(index=False))
+        pointer.setposition(x, y)
+        pointer.write(voivodeship_name, font=("Arial", 10, "bold"))
+        i += 1
+    else:
+        continue
 
 screen.mainloop()
