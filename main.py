0ea36cd112e01fe477c9324c54201129cecd1935
Bartek <64077991+TheBarr@users.noreply.github.com>
2023-06-30 01:22:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,14 @@ while len(guessed_voivodeships) < 17:
                                               prompt="What's another voivodeship name?").title()
     except AttributeError:
         break
+        
     if answer_voivodeship == "Exit":
         voivodeships_to_learn = [voivodeship for voivodeship in all_voivodeships if
                                  voivodeship not in guessed_voivodeships]
         df = pandas.DataFrame(voivodeships_to_learn, columns=["To Learn"])
         df.to_csv('voivodeships_to_learn.csv', index=False)
         break
+        
     elif answer_voivodeship in all_voivodeships:
         guessed_voivodeships.append(answer_voivodeship)
         voiv = data[data.voivodeship == answer_voivodeship]
